c4acd036d106c3aea50d9c2d4d12694865590999
Rajtew65 <89356341+Rajtew65@users.noreply.github.com>
2021-12-31 00:30:06 +0530

    Create a web application using create-react-app
    Create a web application using create-react-app

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,29 @@
+ // eslint-disable-next-line
+ import React from 'react';
+
+ const Navbar = (props) => {
+ 
+   const loadUsers = async()=>{
+     console.log('before');
+     props.setLoading(true);
+     const response = await fetch("https://reqres.in/api/users?page=1");
+     const jsonresponse = await response.json();
+     console.log(jsonresponse.data);
+     props.setUsers(jsonresponse.data);
+     setTimeout(() => {
+       props.setLoading(false);
+     }, 3000);
+   }
+   return (
+         <div className="navbar" style={{ backgroundColor: "Gray"}}>
+             <div className="brand-and-toggler">
+           <h1 className="navbar-brand" style={{color: "white"}}>UserTracker</h1>
+         <button onClick={loadUsers}>Get Users</button>
+       </div>
+ 
+       </div>
+   );
+ }
+ 
+ export default Navbar;
+ 
